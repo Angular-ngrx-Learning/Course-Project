c4a1963b7b47daa401814554c6a1f2af973e62df
Tamminedi <VTAMMINE@ford.com>
2020-05-13 21:46:17 -0400

    Fetching Recipes and using the resolver

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -4,21 +4,29 @@ import {Recipe} from './recipe.model';
 import {DataStorageService} from '../shared/data-storage.service';
 import {Observable} from 'rxjs';
 import {RecipeService} from './recipe.service';
+import * as fromApp from '../store/app.reducer';
+import * as RecipesActions from '../recipes/store/recipe.actions';
+import {Store} from '@ngrx/store';
+import { Actions, ofType} from '@ngrx/effects';
+import {take} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class RecipesResolverService implements Resolve<Recipe[]> {
-    constructor(private dataStorageService: DataStorageService,
-                private recipeService: RecipeService) {}
+    constructor(/*private dataStorageService: DataStorageService,
+                private recipeService: RecipeService*/ private store: Store<fromApp.AppState>,
+                private actions$: Actions) {}
 
    /* resolve (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     return this.dataStorageService.fetchRecipes();
     }*/
    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Recipe[]> | Promise<Recipe[]> | Recipe[] {
-     const recipes = this.recipeService.getRecipes();
+    /* const recipes = this.recipeService.getRecipes();
      if (recipes.length === 0) {
        return this.dataStorageService.fetchRecipes();
      } else {
        return recipes;
-     }
+     }*/
+    this.store.dispatch(new RecipesActions.FetchRecipes());
+    return this.actions$.pipe(ofType(RecipesActions.SET_RECIPES), take(1));
    }
 }
